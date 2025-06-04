910d3b1eebd4554108ed8e2475aa7531a50089cd
Muhammad Abid <abidroid1@gmail.com>
2025-06-04 15:29:20 +0500

    navigation

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,25 +1,27 @@
-import { createStackNavigator } from "@react-navigation/stack";
-import LandingScreen from "../screens/LandingScreen/LandingScreen";
-import { Routes } from "./Routes";
-import AxiosExampleOneScreen from "../screens/AxiosExampleOne/AxiosExampleOneScreen";
+import {createStackNavigator} from '@react-navigation/stack';
+import LandingScreen from '../screens/LandingScreen/LandingScreen';
+import {Routes} from './Routes';
+import AxiosExampleOneScreen from '../screens/AxiosExampleOne/AxiosExampleOneScreen';
+import AxiosMovieSearchScreen from '../screens/AxiosMovieSearch/AxiosMovieSearchScreen';
 
 const Stack = createStackNavigator();
 
 const MainNavigation = () => {
-
-    return <Stack.Navigator
-    initialRouteName={Routes.Landing}
-    screenOptions={{header: () => null, headerShown: false}}
-    >
-        <Stack.Screen
-            name={Routes.Landing}
-            component={LandingScreen}
-        />
-        <Stack.Screen
-            name={Routes.AxiosExampleOne}
-            component={AxiosExampleOneScreen}
-        />
-    </Stack.Navigator>;
+  return (
+    <Stack.Navigator
+      initialRouteName={Routes.Landing}
+      screenOptions={{header: () => null, headerShown: false}}>
+      <Stack.Screen name={Routes.Landing} component={LandingScreen} />
+      <Stack.Screen
+        name={Routes.AxiosExampleOne}
+        component={AxiosExampleOneScreen}
+      />
+      <Stack.Screen
+        name={Routes.AxiosMovieSearch}
+        component={AxiosMovieSearchScreen}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export default MainNavigation;
--- navigation/Routes.js
+++ navigation/Routes.js
@@ -1,5 +1,8 @@
+
 export const Routes = {
 
     Landing: 'LandingScreen',
     AxiosExampleOne: 'AxiosExampleOneScreen',
+        AxiosMovieSearch: 'AxiosMovieSearch',
+
 };
