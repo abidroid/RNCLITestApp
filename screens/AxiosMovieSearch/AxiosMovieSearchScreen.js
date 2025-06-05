69ee26d969c8b8eaf3bb4abd43431eb0af2414a0
Muhammad Abid <abidroid1@gmail.com>
2025-06-05 17:35:34 +0500

    movie search screen

--- screens/AxiosMovieSearch/AxiosMovieSearchScreen.js
+++ screens/AxiosMovieSearch/AxiosMovieSearchScreen.js
@@ -1,13 +1,40 @@
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Text} from 'react-native';
+import {Alert, Text, TextInput, TouchableOpacity} from 'react-native';
+import {useState} from 'react';
 import style from './style';
-import { TextInput } from 'react-native-gesture-handler';
+import axios from 'axios';
 
 const AxiosMovieSearchScreen = () => {
+  const [movieName, setMovieName] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [movie, setMove] = useState({});
+
+  const getMovieDetails = async () => {
+    setLoading(true);
+    
+  };
+
   return (
     <SafeAreaView style={style.container}>
       <Text>Movie Search</Text>
-      <TextInput style={style.input}/>
+      <TextInput
+        style={style.input}
+        placeholder="movie name"
+        value={movieName}
+        onChangeText={text => setMovieName(text)}
+      />
+      <TouchableOpacity
+        style={style.button}
+        onPress={() => {
+          console.log(movieName);
+          if( movieName === ''){
+
+            Alert.alert('Please provide movie name');
+          }
+        }}>
+        <Text style={style.buttonText}>Search</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
--- screens/AxiosMovieSearch/style.js
+++ screens/AxiosMovieSearch/style.js
@@ -2,9 +2,22 @@ import { StyleSheet } from "react-native";
 
 const style = StyleSheet.create({
 
-    container: {flex: 1, padding: 8,},
+    container: {flex: 1, padding: 8, gap:8},
     input: {
         borderWidth: 1,
+        borderRadius:8,
+        paddingHorizontal: 8,
+    },
+    button: {
+        backgroundColor: 'green',
+        padding: 8,
+        alignContent: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        borderRadius: 8,
+    },
+    buttonText: {
+        color: 'white',
     },
 });
 
