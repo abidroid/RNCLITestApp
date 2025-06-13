3c792cc17929abbc48fc20a5e9cb514ce08bb525
Muhammad Abid <abidroid1@gmail.com>
2025-06-13 22:46:17 +0500

    fixes

--- screens/RadioButtonExample/RadioButtonExampleScreen.js
+++ screens/RadioButtonExample/RadioButtonExampleScreen.js
@@ -1,7 +1,11 @@
-
+import {View, Text} from 'react-native';
 
 const RadioButtonExampleScreen = () => {
-
+    return (
+        <View>
+            <Text>Radio Example</Text>
+        </View>
+    );
 };
 
 export default RadioButtonExampleScreen;
