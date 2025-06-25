6f7302730440e478ab63eedd20d49a7c7690e9fa
Muhammad Abid <abidroid1@gmail.com>
2025-06-25 09:47:50 +0500

    Reactotron configured

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import './gesture-handler';
  * @format
  */
 
+if (__DEV__) {
+  require("./ReactotronConfig");
+}
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
